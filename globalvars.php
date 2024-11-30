2c09af0f1930b50b8315bfd349391f6eedfd8feb
bpaltezo <dev.bpaltezo@gmail.com>
2024-12-01 07:23:17 +0800

    global vars

---
+++ globalvars.php
@@ -0,0 +1,15 @@
+<?php
+
+    // PUT THIS IN THE PARENT DIRECTORY
+
+    date_default_timezone_set('Asia/Manila');
+
+    define('DBUSER','');
+    define('DBPWD','');
+    define('DBHOST','');
+    define('DBNAME','');
+
+    ini_set('display_errors', 1);
+    error_reporting(-1);
+
+?>
